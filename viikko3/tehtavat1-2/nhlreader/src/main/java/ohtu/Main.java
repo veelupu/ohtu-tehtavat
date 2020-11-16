bb541fd9417147e699b071ffea0100550ba27b26
Veera Lupunen <veera.lupunen@helsinki.fi>
2020-11-16 19:50:31 +0200

    viikko3

---
+++ viikko3/tehtavat1-2/nhlreader/src/main/java/ohtu/Main.java
@@ -0,0 +1,35 @@
+package ohtu;
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import org.apache.http.client.fluent.Request;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String url = "https://nhlstatisticsforohtu.herokuapp.com/players";
+        
+        String bodyText = Request.Get(url).execute().returnContent().asString();
+                
+        //System.out.println("json-muotoinen data:");
+        //System.out.println( bodyText );
+
+        Gson mapper = new Gson();
+        Player[] players = mapper.fromJson(bodyText, Player[].class);
+        
+        System.out.println("Players from Finland");
+	ArrayList<Player> plist = new ArrayList<>();
+        for (Player player : players) {
+            if (player.getNationality().equals("FIN")) {
+		plist.add(player);
+       	    }
+	 }   
+	Collections.sort(plist);
+        Collections.reverse(plist);
+        for (Player player : plist) {
+            System.out.println(player);
+        }
+    }
+  
+}
